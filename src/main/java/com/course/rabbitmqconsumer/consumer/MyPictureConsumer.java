75defa6b755550f38b71c83c27d3c73a294b9588
mecong <mecongsiam@gmail.com>
2021-01-10 21:03:36 +0300

    dlx example 2  with acknowledge-mode: manual implemented

--- src/main/java/com/course/rabbitmqconsumer/consumer/MyPictureConsumer.java
+++ src/main/java/com/course/rabbitmqconsumer/consumer/MyPictureConsumer.java
@@ -3,11 +3,16 @@ package com.course.rabbitmqconsumer.consumer;
 import com.course.rabbitmqconsumer.entity.Picture;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.rabbitmq.client.Channel;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.amqp.AmqpRejectAndDontRequeueException;
+
+import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
+
+import java.io.IOException;
 
 @Component
 public class MyPictureConsumer {
@@ -21,15 +26,17 @@ public class MyPictureConsumer {
     }
 
     @RabbitListener(queues = "q.mypicture.image")
-    public void getMessage(String message) throws JsonProcessingException {
+    public void getMessage(Message message, Channel channel) throws IOException {
 
-        Picture picture = objectMapper.readValue(message, Picture.class);
+        Picture picture = objectMapper.readValue(message.getBody(), Picture.class);
 
         logger.info("Image picture consumed: {}", picture);
 
         if (picture.getSize() > 9000L) {
-            throw new AmqpRejectAndDontRequeueException("Size more then 9000");
+            channel.basicReject(message.getMessageProperties().getDeliveryTag(), false);
         }
+
+        channel.basicAck(message.getMessageProperties().getDeliveryTag(), false);
 
     }
 }
